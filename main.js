de2069f623eb5c11dc476634247e81c1835133be
ManvikR <110921768+ManvikR@users.noreply.github.com>
2023-01-04 20:59:34 -0800

    Update main.js

--- main.js
+++ main.js
@@ -12,18 +12,39 @@ recognition.onresult = function(event) {
     var Content = event.results[0][0].transcript;
 
     document.getElementById("textbox").innerHTML = Content;
-    speak();
+    console.log(Content);
+    if(Content=="take my selfie")
+    {
+        console.log("taking selfie --- ");
+        speak();
+    }
 }
+
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
+
 
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your Selfie in 5 seconds";
 
     var utter_this = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utter_this);
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    },5000);
 
 }
 
@@ -34,3 +55,10 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img> id="selfie_image" src="'+data_uri+'">';
+    });
+}
